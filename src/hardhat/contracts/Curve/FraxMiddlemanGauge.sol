2e73ca1503655417f0170703bbf0a11a954ae9aa
juliustranquilli <kingofthecoast78@gmail.com>
2024-12-14 11:12:53 -0500

    update src/hardhat/contracts/Curve/FraxMiddlemanGauge.sol: refactored SafeMath expressions

--- src/hardhat/contracts/Curve/FraxMiddlemanGauge.sol
+++ src/hardhat/contracts/Curve/FraxMiddlemanGauge.sol
@@ -25,7 +25,6 @@ pragma solidity >=0.6.11;
 // Sam Kazemian: https://github.com/samkazemian
 
 import "../Math/Math.sol";
-import "../Math/SafeMath.sol";
 import "../ERC20/ERC20.sol";
 import "../ERC20/SafeERC20.sol";
 import "./IFraxGaugeFXSRewardsDistributor.sol";
@@ -37,7 +36,6 @@ import "../Staking/Owned.sol";
 import "../Utils/ReentrancyGuard.sol";
 
 contract FraxMiddlemanGauge is Owned, ReentrancyGuard {
-    using SafeMath for uint256;
     using SafeERC20 for ERC20;
 
     /* ========== STATE VARIABLES ========== */
