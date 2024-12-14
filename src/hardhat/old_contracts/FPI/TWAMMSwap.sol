610b8361dc59b5004f4a9ce2f2cae6c413407c3f
juliustranquilli <kingofthecoast78@gmail.com>
2024-12-14 11:13:05 -0500

    update src/hardhat/old_contracts/FPI/TWAMMSwap.sol: refactored SafeMath expressions

--- src/hardhat/old_contracts/FPI/TWAMMSwap.sol
+++ src/hardhat/old_contracts/FPI/TWAMMSwap.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.4;
 
 import "./Math.sol";
-import "./SafeMath.sol";
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -70,8 +69,8 @@ contract TWAMMSwap is Ownable {
         buy1EndTime = _buy1EndTime;
         twammTimestampLast=block.timestamp;
         
-        uint balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        uint balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        uint balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        uint balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
         _update(balance0, balance1);
     }
     
@@ -89,32 +88,32 @@ contract TWAMMSwap is Ownable {
     // Removes liquidity
     function removeLiquidity(uint percentage) external lock onlyOwner returns (uint amount0, uint amount1) {
         _executeTWAMMTrade();
-        uint balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        uint balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        uint balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        uint balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
 
         amount0 = balance0*percentage/1e18;
         amount1 = balance1*percentage/1e18;
         SafeERC20.safeTransfer(token0, msg.sender, amount0);
         SafeERC20.safeTransfer(token1, msg.sender, amount1);
         
-        balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
         _update(balance0, balance1);
     }
     
      // Adds liquidity
     function addLiquidity(uint token0Amount, uint token1Amount, uint min_token1Amount, uint max_token1Amount) external lock onlyOwner {
         _executeTWAMMTrade();
-        uint balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        uint balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        uint balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        uint balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
         
         if (balance1>0) token1Amount = balance0*balance1/token0Amount; // adjust token1Amount to match the ratio
         require(token1Amount>=min_token1Amount && token1Amount<=max_token1Amount,'TWAPP: PRICE NOT WITHIN BOUNDS');
         SafeERC20.safeTransferFrom(token0, msg.sender, address(this), token0Amount);
         SafeERC20.safeTransferFrom(token1, msg.sender, address(this), token1Amount);
         
-        balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
         _update(balance0, balance1);
     }
     
@@ -124,8 +123,8 @@ contract TWAMMSwap is Ownable {
     function swapExactTokensForTokens(uint amountIn, uint amountOutMin, IERC20 tokenFrom, IERC20 tokenTo, address to, uint deadline) external lock returns (uint amount) {
         require(deadline==0 || block.timestamp<=deadline,'TWAPP: EXPIRED');
         _executeTWAMMTrade();
-        uint balance0 = IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        uint balance1 = IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        uint balance0 = IERC20(token0).balanceOf(address(this)) + sold0 - bought0;
+        uint balance1 = IERC20(token1).balanceOf(address(this)) + sold1 - bought1;
         
         if (tokenFrom==token0  && tokenTo==token1) {
             amount = getAmountOut(amountIn, balance0, balance1);
@@ -147,14 +146,14 @@ contract TWAMMSwap is Ownable {
     function skim(address to) external lock {
         IERC20 _token0 = token0; // gas savings
         IERC20 _token1 = token1; // gas savings
-        SafeERC20.safeTransfer(_token0, to, _token0.balanceOf(address(this)).add(bought0).sub(sold0).sub(reserve0));
-        SafeERC20.safeTransfer(_token1, to, _token1.balanceOf(address(this)).add(bought1).sub(sold1).sub(reserve1));
+        SafeERC20.safeTransfer(_token0, to, _token0.balanceOf(address(this)) + bought0 - sold0 - reserve0);
+        SafeERC20.safeTransfer(_token1, to, _token1.balanceOf(address(this)) + bought1 - sold1 - reserve1);
     }
 
     // force reserves to match balances
     function sync() external lock {
     	_executeTWAMMTrade();
-        _update(IERC20(token0).balanceOf(address(this)).add(sold0).sub(bought0), IERC20(token1).balanceOf(address(this)).add(sold1).sub(bought1));
+        _update(IERC20(token0).balanceOf(address(this)) + sold0 - bought0, IERC20(token1).balanceOf(address(this)) + sold1 - bought1);
     }
     
     
@@ -204,16 +203,16 @@ contract TWAMMSwap is Ownable {
         if (amount1Out > 0) SafeERC20.safeTransfer(_token1, to, amount1Out); // optimistically transfer tokens
         
         //if (data.length > 0) IUniswapV2Callee(to).uniswapV2Call(msg.sender, amount0Out, amount1Out, data);
-        balance0 = IERC20(_token0).balanceOf(address(this)).add(sold0).sub(bought0);
-        balance1 = IERC20(_token1).balanceOf(address(this)).add(sold1).sub(bought1);
+        balance0 = IERC20(_token0).balanceOf(address(this)) + sold0 - bought0;
+        balance1 = IERC20(_token1).balanceOf(address(this)) + sold1 - bought1;
         }
         uint amount0In = balance0 > _reserve0 - amount0Out ? balance0 - (_reserve0 - amount0Out) : 0;
         uint amount1In = balance1 > _reserve1 - amount1Out ? balance1 - (_reserve1 - amount1Out) : 0;
         require(amount0In > 0 || amount1In > 0, 'TWAPP: INSUFFICIENT_INPUT_AMOUNT');
         { // scope for reserve{0,1}Adjusted, avoids stack too deep errors
-        uint balance0Adjusted = balance0.mul(1000).sub(amount0In.mul(3));
-        uint balance1Adjusted = balance1.mul(1000).sub(amount1In.mul(3));
-        require(balance0Adjusted.mul(balance1Adjusted) >= uint(_reserve0).mul(_reserve1).mul(1000**2), 'TWAPP: K');
+        uint balance0Adjusted = balance0 * 1000 - amount0In * 3;
+        uint balance1Adjusted = balance1 * 1000 - amount1In * 3;
+        require(balance0Adjusted * balance1Adjusted >= uint(_reserve0) * _reserve1 * 1000**2, 'TWAPP: K');
         }
         _update(balance0, balance1);
         emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
@@ -225,9 +224,9 @@ contract TWAMMSwap is Ownable {
     function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut) internal pure returns (uint amountOut) {
         require(amountIn > 0, 'TWAPP: INSUFFICIENT_INPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'TWAPP: INSUFFICIENT_LIQUIDITY');
-        uint amountInWithFee = amountIn.mul(997);
-        uint numerator = amountInWithFee.mul(reserveOut);
-        uint denominator = reserveIn.mul(1000).add(amountInWithFee);
+        uint amountInWithFee = amountIn * 997;
+        uint numerator = amountInWithFee * reserveOut;
+        uint denominator = reserveIn * 1000 + amountInWithFee;
         amountOut = numerator / denominator;
     }
     
@@ -235,8 +234,8 @@ contract TWAMMSwap is Ownable {
     function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut) internal pure returns (uint amountIn) {
         require(amountOut > 0, 'TWAPP: INSUFFICIENT_OUTPUT_AMOUNT');
         require(reserveIn > 0 && reserveOut > 0, 'TWAPP: INSUFFICIENT_LIQUIDITY');
-        uint numerator = reserveIn.mul(amountOut).mul(1000);
-        uint denominator = reserveOut.sub(amountOut).mul(997);
-        amountIn = (numerator / denominator).add(1);
+        uint numerator = reserveIn * amountOut * 1000;
+        uint denominator = reserveOut - amountOut * 997;
+        amountIn = (numerator / denominator) + 1;
     }
 }
