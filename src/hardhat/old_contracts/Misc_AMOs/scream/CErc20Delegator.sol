fe63e9931a235095db466201b7a125a839e29819
juliustranquilli <kingofthecoast78@gmail.com>
2024-12-14 11:13:05 -0500

    update src/hardhat/old_contracts/Misc_AMOs/scream/CErc20Delegator.sol: refactored SafeMath expressions

--- src/hardhat/old_contracts/Misc_AMOs/scream/CErc20Delegator.sol
+++ src/hardhat/old_contracts/Misc_AMOs/scream/CErc20Delegator.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity ^0.5.17;
+pragma solidity ^0.8.0;
 
 contract ComptrollerInterface {
     /// @notice Indicator that this is a Comptroller contract (for inspection)
